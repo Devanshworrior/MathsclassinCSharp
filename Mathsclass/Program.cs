8959ab816bc7134bbf2efc9fe8e180c7cb21aa3a
Devanshworrior <devanshjangid269@gmail.com>
2024-04-16 20:56:29 +0530

    first commit

---
+++ Mathsclass/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mathsclass
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Math.Max(34, 345);
+            Console.WriteLine(a);
+            int b = Math.Min(34, 345);
+            Console.WriteLine(b);
+            double c = Math.Sqrt(36);
+            Console.WriteLine(c);
+            int d = Math.Abs( - 78);
+            Console.WriteLine(d);
+            Console.ReadLine();
+
+        }
+    }
+}
